9faebec06df39c699f8f5f68aa9ff1b431bf8c6e
Sergio Rey <reysergio383@gmail.com>
2022-03-19 23:45:58 -0500

    Second commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,21 @@
-alert("Hola Mundo")
-
+//Create a Canvas
 function setup () {
     createCanvas(400, 400)
 }
 
 function draw () {
-    background(220)
+    background(155, 236, 87)
+
+    // Face
+
+    /*rect(100, 50, 50, 50, 20)*/
+    /*rect(250, 50, 50, 50, 20)*/
+    /*rect(100, 250, 200, 50)*/
+    
+    // Curve
+
+    /*for (var i = 0; i <= 200; i++) {*/
+	/*line(0, 0 + (i * 2), 0 + (i * 2), 400)*/
+    /*}*/
+
 }
